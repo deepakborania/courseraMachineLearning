a8ba60847752a9020d0ac16fb9b6d0c0e95951a5
deepakborania <deepakborania@users.noreply.github.com>
2015-09-30 00:41:24 +0530

    All exercises except learning rate

--- octave/machine-learning-ex1/ex1/computeCostMulti.m
+++ octave/machine-learning-ex1/ex1/computeCostMulti.m
@@ -3,20 +3,13 @@ function J = computeCostMulti(X, y, theta)
 %   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
-m = length(y); % number of training examples
+  % Initialize some useful values
+  m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+  % You need to return the following variables correctly 
+  J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
-
-
-
-
-% =========================================================================
+  %Should have thought of this earlier.
+  J = sum(((X * theta)-y)'*((X * theta)-y))/(2*m);
 
 end
--- octave/machine-learning-ex1/ex1/gradientDescent.m
+++ octave/machine-learning-ex1/ex1/gradientDescent.m
@@ -7,27 +7,25 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+tic
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
-
-    % ============================================================
+    %This raises warning. What was I thinking?
+    %theta = theta - (alpha*(1/m)*sum((X*theta - y).*(X)))';
+    
+    %Using loops : 0.215708 seconds
+%    temp = zeros(length(theta),1);
+%    for i = 1:length(theta)
+%      temp(i) = theta(i) - (alpha*(1/m)*sum((X*theta - y).*X(:,i)));
+%    end
+%    theta = temp;
+    
+    %Using bsxfun : 0.103053 seconds
+    theta = theta - (alpha*(1/m)*sum(bsxfun(@times,(X*theta - y),X)))';
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
 end
-
+toc
 end
